d185e7c1bf5b7ee3f92986039acc918943225004
Agata Musiuk <agata.musiuk@gmail.com>
2019-12-15 11:52:08 +0100

    Changed config vars and server for heroku

--- server/config.js
+++ server/config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    PORT: 8000,
-    DB: 'mongodb+srv://agat:Perseverance92@cluster0-kvp2j.mongodb.net/test?retryWrites=true&w=majority',
+    PORT: process.env.PORT || 8000,
+    DB: process.env.DB,
 };
--- server/server.js
+++ server/server.js
@@ -1,6 +1,3 @@
-//import routes
-const itemRoutes = require('./routes/item.routes');
-
 const express = require('express');
 const cors = require('cors');
 const config = require('./config');
@@ -8,8 +5,15 @@ const mongoose = require('mongoose');
 const helmet = require('helmet');
 const sanitize = require('mongo-sanitize');
 const loadTestData = require('./testData');
+const path = require('path');
 
 const app = express();
+
+//import routes
+const itemRoutes = require('./routes/item.routes');
+
+//serve static files from react app
+app.use(express.static(path.join(__dirname, '/../client/build')));
 
 app.use(cors());
 app.use(helmet());
@@ -20,6 +24,10 @@ app.use('/api', itemRoutes);
 app.use((req, res, next) => {
     sanitize(req.body);
     next();
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/../client/build/index.html'));
 });
 
 //connects backend code with the database
